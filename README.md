fc717e87231ff4851dbca73349b73b06d4fe3e00
rithik1024 <rithikraj1024@gmail.com>
2024-03-08 18:23:48 +0530

    new commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AWS Configurations Required
+
+- To run terraform create service account : svc-terraform-eks-deploy 
+- Create the iam policies (see json files) and attach to the service account
+- Attach Iam Full access policy to the service account
---
+++ variables.tf
@@ -0,0 +1,15 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  
+}
